14fffa2ddc0c27798429280356e6bf868d79d8e5
kelvindaudi <kelvindaudi98@gmail.com>
2023-12-03 04:49:04 -0800

    python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    chars = [x for x in my_string]
-
-    for char in chars:
-        if char == "c" or char == "C":
-            chars.remove(char)
-        new_string = ''.join(chars)
+    new_string = ""
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            new_string += elements
     return new_string
